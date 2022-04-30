2ca1f94658c4d0c33f6c6aad61aa2a7d9967a21a
Alexander Koskovich <akoskovich@pm.me>
2022-05-01 03:20:11 +0530

    haydn: Remove vendor RenderScript implementation.
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: I19460ef266a646b046f1e7d2f0b4eab7c48ae536

--- device.mk
+++ device.mk
@@ -339,10 +339,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
